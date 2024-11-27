1afac3367f1e02ddacb33ed21d9a200eb815b2f2
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-11-27 10:57:11 +0900

    Delete src directory

--- src/types/bcrypt.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'bcrypt' {
-    export function compare(data: string | Buffer, encrypted: string): Promise<boolean>;
-    export function hash(data: string | Buffer, saltOrRounds: number): Promise<string>;
-  }
-  

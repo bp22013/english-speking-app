a16cf2172d16ce27c4c5f3f6bd5e871d5d7aba31
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-11-27 11:05:01 +0900

    Update page.tsx

--- src/app/Admindashboard/Notification/page.tsx
+++ src/app/Admindashboard/Notification/page.tsx
@@ -1,3 +1,14 @@
 /* お知らせ作成ページ */
 
 'use client';
+
+import { NextPage } from 'next';
+
+const NotificationPage: NextPage = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default NotificationPage

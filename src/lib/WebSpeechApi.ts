7e82005413d6de11752e329c9e27c1b44d85f269
BP22013 <bp22013@shibaura-it.ac.jp>
2024-12-05 05:16:39 +0900

    スピーク機能の追加

--- src/lib/WebSpeechApi.ts
+++ src/lib/WebSpeechApi.ts
@@ -2,15 +2,10 @@
 
 import toast from "react-hot-toast";
 
-export const speak = (text: string, speed: number) => {
+export const speak = (text: string) => {
     if ('speechSynthesis' in window) {
         const utterance = new SpeechSynthesisUtterance(text);
         utterance.lang = 'en-US'; // 英語（アメリカ）
-        
-        // speed 引数で音声速度を調整
-        // rate の値は 0.1 から 10.0 までが有効範囲
-        utterance.rate = Math.max(0.1, Math.min(speed, 10));
-
         speechSynthesis.speak(utterance);
     } else {
         toast.error('このブラウザでは合成音声は使用できません');

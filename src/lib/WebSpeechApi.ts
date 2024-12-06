19b72d4ae53fedd7e11ae514862fe1530d0f3038
BP22013 <bp22013@shibaura-it.ac.jp>
2024-12-06 15:00:00 +0900

    web speech apiの速さ追加

--- src/lib/WebSpeechApi.ts
+++ src/lib/WebSpeechApi.ts
@@ -6,6 +6,7 @@ export const speak = (text: string) => {
     if ('speechSynthesis' in window) {
         const utterance = new SpeechSynthesisUtterance(text);
         utterance.lang = 'en-US'; // 英語（アメリカ）
+        utterance.rate = 1.0
         speechSynthesis.speak(utterance);
     } else {
         toast.error('このブラウザでは合成音声は使用できません');
